a960f0f2ea18e7cb53f454bd638c64cf38d25be0
Nicholas Levenson <nicklevenson@gmail.com>
2020-12-16 13:18:33 -0800

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,14 +1,18 @@
 class SchoolClassesController < ApplicationController
 
   def show
+    @school_class = SchoolClass.find(params[:id])
   end
   def new
+    @school_class = SchoolClass.new
   end
   def create
+    @school_class = SchoolClass.create(params.require(:school_class).permit(:title, :room_number))
+    redirect_to school_class_path(@school_class)
   end
   def edit
   end
   def update
   end
-  
+
 end
